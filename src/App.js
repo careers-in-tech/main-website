b91dba94c1c6cfd18b979532e1cdc2cf03699d8c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-08-14 01:14:16 -0600

    Slider Text Content Added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <hr />
         <h1 className="headerTitle">Careers in Tech and Innovation</h1>
         <p className="headerText">Helping you find your future in tech</p>
-        <button>Become a Member</button>
-        <button>Become an Advisor</button>
+        <div>
+          <button className="member">Button Component</button>
+          <button className="advisor">Button Component</button>
+        </div>
       </header>
     </div>
   );
