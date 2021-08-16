23182bff1e828c29ce9b604b13a7d49894ff5144
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2021-08-16 01:20:39 -0600

    Added Box/Text Shadows

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,12 @@ import "./button.css";
 const Button = (props) => {
   return (
     <div>
-      <a className={`button ${props.color}`} href={props.link} target="_blank">
+      <a
+        className={`button ${props.color}`}
+        href={props.link}
+        target="_blank"
+        rel="noreferrer"
+      >
         {props.text}
       </a>
     </div>
